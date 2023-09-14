d178a885327e5da9dae595f1cb84a5469ad62450
shankar <shankarpenjarla1998@gmail.com>
2023-09-14 04:47:47 -0700

    instancecreation

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 names=("web" "mongodb" "catalogue" "redis" "user" "cart" "mysql" "shipping" "rabbitmq" "payment" "dispatch")
+instance_type=""
+image_id=ami-03265a0778a880afb
+sec_grp_id=sg-07eac0a2eb62d0655
 
+#if mysql or mongodb instance type should be t2.small
 for i in "${names[@]}"
 do 
-echo "NAME:$i"
+    if [[ $i == "mongodb" || $i == "mysql"  ]]
+    then
+       instance_type="t2.small"
+    else
+       instance_type="t2.micro"
+     fi
+  echo "creating $i instance"
+  aws ec2 run-instances --image-id $image_id  --count 1 --instance-type t2.micro  --security-group-ids $sec_grp_id "ResouceType=instance,Tags=[{key=Name,Value=$i}]"
 done
