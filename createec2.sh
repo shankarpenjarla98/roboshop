536c27aae0409d5878176d0d8d5d8777c0da7518
shankar <shankarpenjarla1998@gmail.com>
2023-09-14 06:06:55 -0700

    records

--- createec2.sh
+++ createec2.sh
@@ -11,14 +11,14 @@ domname=learndevops.uno
 
 for i in "${names[@]}"
 do 
- if [[ $i == "mongodb" || $i == "mysql"]]
- then
-   instance_type="t2.small"
+   if [[ $i == "mongodb" || $i == "mysql"]]
+     then
+       instance_type="t2.small"
    else
-   instance_type="t2.micro"
+       instance_type="t2.micro"
    fi
  echo "creating $i instances"
- ipadd=(aws ec2 run-instances --image-id $imageid --count 1 --instance-type t2.micro  --security-group-ids $securityid  | jq -r '.Instances[0].PrivateIpAddress')
+ ipadd=$(aws ec2 run-instances --image-id $imageid --count 1 --instance-type t2.micro  --security-group-ids $securityid  | jq -r '.Instances[0].PrivateIpAddress')
  echo "created $i instances: $ipadd"
 
  aws route53 change-resource-record-sets --hosted-zone-id Z1037665238BF3QDUCKFN --change-batch '
