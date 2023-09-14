42d5553ed063234d6faf4c06607f646af4272b12
shankar <shankarpenjarla1998@gmail.com>
2023-09-14 06:22:25 -0700

    ec2's

--- ec2.sh
+++ ec2.sh
@@ -12,5 +12,4 @@ do
      inst_type="t2.micro"
  echo "$i instance created"
   aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type t2.micro  --security-group-ids sg-07eac0a2eb62d0655
- 
-     
+ done
